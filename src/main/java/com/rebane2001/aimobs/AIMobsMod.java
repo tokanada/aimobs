a26686596ce808cc508709d6168cb9e8499be869
tokanada <davidt.3200@gmail.com>
2022-12-25 17:09:17 -0800

    Port Mod over to 1.18.2

---
+++ src/main/java/com/rebane2001/aimobs/AIMobsMod.java
@@ -0,0 +1,51 @@
+package com.rebane2001.aimobs;
+
+import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.util.ActionResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.argument;
+
+public class AIMobsMod implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("aimobs");
+
+	@Override
+	public void onInitializeClient() {
+		AIMobsConfig.loadConfig();
+		ClientCommandManager.DISPATCHER.register(
+				ClientCommandManager.literal("aimobs")
+				.executes(AIMobsCommand::status)
+				.then(ClientCommandManager.literal("help").executes(AIMobsCommand::help))
+				.then(ClientCommandManager.literal("setkey")
+						.then(argument("key", StringArgumentType.string())
+								.executes(AIMobsCommand::setAPIKey)
+						))
+				.then(ClientCommandManager.literal("setmodel")
+						.then(argument("model", StringArgumentType.string())
+								.executes(AIMobsCommand::setModel)
+						))
+				.then(ClientCommandManager.literal("settemp")
+						.then(argument("temperature", FloatArgumentType.floatArg(0,1))
+								.executes(AIMobsCommand::setTemp)
+						))
+				.then(ClientCommandManager.literal("enable").executes(context -> AIMobsCommand.setEnabled(context, true)))
+				.then(ClientCommandManager.literal("disable").executes(context -> AIMobsCommand.setEnabled(context, false))
+				)
+		);
+		AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			if (!AIMobsConfig.config.enabled) return ActionResult.PASS;
+			if (!player.isSneaking()) {
+				if (entity.getId() == ActionHandler.entityId)
+					ActionHandler.handlePunch(entity, player);
+				return ActionResult.PASS;
+			}
+			ActionHandler.startConversation(entity, player);
+			return ActionResult.FAIL;
+		});
+	}
+}
---
+++ src/main/java/com/rebane2001/aimobs/RequestHandler.java
@@ -0,0 +1,58 @@
+package com.rebane2001.aimobs;
+
+import com.google.gson.Gson;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class RequestHandler {
+    private static class OpenAIRequest {
+        String model = "text-davinci-002";
+        String stop = "\"";
+        String prompt = "";
+        float temperature = 0.6f;
+        int max_tokens = 64;
+
+        OpenAIRequest(String prompt) {
+            this.prompt = prompt;
+        }
+        OpenAIRequest(String prompt, String model, float temperature) {
+            this.prompt = prompt;
+            this.model = model;
+            this.temperature = temperature;
+        }
+    }
+
+    private static class OpenAIResponse {
+        static class Choice {
+            String text;
+        }
+        Choice[] choices;
+    }
+
+    public static String getAIResponse(String prompt) throws IOException {
+        if (prompt.length() > 4096) prompt = prompt.substring(prompt.length() - 4096);
+        AIMobsMod.LOGGER.info("Prompt: " + prompt);
+
+        OpenAIRequest openAIRequest = new OpenAIRequest(prompt, AIMobsConfig.config.model, AIMobsConfig.config.temperature);
+        String data = new Gson().toJson(openAIRequest);
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpPost request = new HttpPost("https://api.openai.com/v1/completions");
+            StringEntity params = new StringEntity(data, "UTF-8");
+            request.addHeader("Content-Type", "application/json");
+            request.addHeader("Authorization", "Bearer " + AIMobsConfig.config.apiKey);
+            request.setEntity(params);
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+            String responseString = EntityUtils.toString(entity, "UTF-8");
+            return new Gson().fromJson(responseString, OpenAIResponse.class).choices[0].text.replace("\n", " ");
+        }
+    }
+}
